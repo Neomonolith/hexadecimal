52a58be92990f89202426c9605d1cd0f5ad8b27a
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2021-10-07 11:16:12 +0200

    Add project files.

---
+++ hexadecimal/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hexaddecimal
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Kérek egy kétjegyű hexadecimális számot");
+
+      //string hexa = Console.ReadLine();
+      for (int i = 0; i < 5; i++)
+      {
+
+
+
+
+        string hexString = Console.ReadLine();
+        int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+        Console.WriteLine($"Hexadecimális megfelelője: {num}");
+
+
+
+        /*
+
+        string hexa = Console.ReadLine();
+        // this returns 1322173
+        int intValue = int.Parse(hexa, System.Globalization.NumberStyles.HexNumber);
+        Console.WriteLine(intValue);
+
+        */
+      }
+
+      Console.ReadKey();
+    }
+
+  }
+}
